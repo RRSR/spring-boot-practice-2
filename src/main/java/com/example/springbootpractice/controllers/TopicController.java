08fee7e4ad97ca760f395806fafc08e10427566c
RRSR <rajrajeshwarsingh.rathore@practo.com>
2024-02-04 21:23:40 +0530

    Used constructor injection instead of field injection for mandatory dependency.

--- src/main/java/com/example/springbootpractice/controllers/TopicController.java
+++ src/main/java/com/example/springbootpractice/controllers/TopicController.java
@@ -4,7 +4,6 @@ package com.example.springbootpractice.controllers;
 import java.util.List;
 import com.example.springbootpractice.model.Topic;
 import com.example.springbootpractice.service.TopicService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/topics")
 public class TopicController {
 
-  @Autowired
-  TopicService topicService;
-  
+  private final TopicService topicService;
+
+  public TopicController(TopicService topicService) {
+    this.topicService = topicService;
+  }
+
   @GetMapping(value = "/list")
   public List<Topic> getAllTopics() {
     return topicService.getAllTopics();
