469e50a9fc4a3de4276941f9f8aabd78cb4666d0
RRSR <rajrajeshwarsingh.rathore@practo.com>
2024-02-04 20:40:50 +0530

    Updated to use `@RestController` instead of `@Controller` and got rid of the `@ResponseBody`

--- src/main/java/com/example/springbootpractice/controllers/TopicController.java
+++ src/main/java/com/example/springbootpractice/controllers/TopicController.java
@@ -5,20 +5,19 @@ import java.util.List;
 import com.example.springbootpractice.model.Topic;
 import com.example.springbootpractice.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/topics")
 public class TopicController {
 
   @Autowired
   TopicService topicService;
-
+  
   @GetMapping(value = "/list")
-  public @ResponseBody List<Topic> getAllTopics() {
+  public List<Topic> getAllTopics() {
     return topicService.getAllTopics();
   }
 
