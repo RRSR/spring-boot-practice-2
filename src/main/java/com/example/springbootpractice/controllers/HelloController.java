187df4cbba4251ef865d2fa647ce0f8a7b94df8d
RRSR <rajrajeshwarsingh.rathore@practo.com>
2024-02-04 20:09:50 +0530

    Added a `PostMapping`.

--- src/main/java/com/example/springbootpractice/controllers/HelloController.java
+++ src/main/java/com/example/springbootpractice/controllers/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.springbootpractice.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
-  @RequestMapping(value = "/welcome")
+  @GetMapping("/welcome")
   public String welcome() {
     return "Hello World!";
+  }
+
+  @PostMapping("/welcome")
+  public String welcome(@RequestBody String name) {
+    return "Hello " + name;
   }
 }
